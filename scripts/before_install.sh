2df8011bbe3e0f6d90bb02c753c7e384e0ebb484
Chris Kettenbach <christopherk@logic2020.com>
2022-10-20 17:05:25 -0700

    CICD 25

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,5 +3,5 @@
 # navigate to app folder
 cd /home/ec2-user/nodeapps/nextjs1
 
-echo after_install > after_install.txt
-date >> after_install.txt
+echo before_install > before_install.txt
+date >> before_install.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,13 +2,16 @@
 
 # navigate to app folder
 cd /home/ec2-user/nodeapps/nextjs1
+pwd
 
 echo "Starting Application"
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+echo $NVM_DIR
 nvm use --lts
-/home/ec2-user/.nvm/versions/node/v16.18.0/bin/npm install
-/home/ec2-user/.nvm/versions/node/v16.18.0/bin/npm run build
-/home/ec2-user/.nvm/versions/node/v16.18.0/bin/npm run start
+nvm current
+#npm install
+#npm run start
+
